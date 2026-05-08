75992d6da4fdc10416b9a062d6dfa49758e342a0
Louistatch <louistatch@gmail.com>
2026-05-08 23:00:28 +0000

    fix: 2 workers + fix debug mode off + correct PORT in app.run

--- app/server.py
+++ app/server.py
@@ -431,4 +431,5 @@ def engine_feedback():
 
 
 if __name__ == "__main__":
-    app.run(debug=True, port=5000)
+    port = int(os.environ.get("PORT", 8080))
+    app.run(debug=False, host="0.0.0.0", port=port)
